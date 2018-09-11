dfea8d90dc95d120cf8ec4d264588f09ed92d108
maxpost <max.post.data.analytics@gmail.com>
2018-09-11 16:17:01 +0200

    CV added

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,6 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def cv(request):
+    return render(request, 'blog/cv.html', {})
