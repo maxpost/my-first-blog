13842ecca4fd71d0e75d045460e0df20e367c070
maxpost <max.post.data.analytics@gmail.com>
2018-09-19 12:20:48 +0200

    Resume angepasst und neue Reiter hinzugefügt

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('post', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('xxxx',views.home,name='home'),
+    path('',views.home,name='home'),
     path('resume',views.resume,name='resume'),
     path('apps',views.apps,name='apps'),
 ]
